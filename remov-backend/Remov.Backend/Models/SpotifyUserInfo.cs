0e4445fd3f95988dae8f9e10746bee214560fd08
Plot <49232758+ldstr@users.noreply.github.com>
2024-12-05 11:58:11 +1000

    Create SpotifyUserInfo.cs

---
+++ remov-backend/Remov.Backend/Models/SpotifyUserInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Remov.Backend.Models;
+
+[JsonSerializable(typeof(SpotifyUserInfo[]))]
+internal partial class SpotifyUserInfoContext : JsonSerializerContext;
+
+public class SpotifyUserInfo
+{
+    [JsonPropertyName("token")]
+    public string? Token { get; set; }
+
+    [JsonPropertyName("playlist_ids")]
+    public string[]? PlaylistIds { get; set; }
+}
