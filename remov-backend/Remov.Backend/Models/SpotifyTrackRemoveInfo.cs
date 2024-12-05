19dd9b860c944fabbde531c9ab6fcc3fa010ca11
Plot <49232758+ldstr@users.noreply.github.com>
2024-12-05 16:27:02 +1000

    Create SpotifyTrackRemoveInfo.cs

---
+++ remov-backend/Remov.Backend/Models/SpotifyTrackRemoveInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Remov.Backend.Models;
+
+[JsonSerializable(typeof(SpotifyTrackRemoveInfo))]
+internal partial class SpotifyTrackRemoveContext : JsonSerializerContext;
+
+public class SpotifyTrackRemoveInfo(string uri)
+{
+    [JsonPropertyName("tracks")]
+    public TrackInfo[] Tracks { get; } = [new(uri)];
+
+    public class TrackInfo(string uri)
+    {
+        [JsonPropertyName("uri")]
+        public string Uri { get; } = uri;
+    }
+}
