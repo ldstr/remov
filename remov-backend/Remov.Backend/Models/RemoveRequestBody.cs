6094c6ac071046354d20dc93972bdce04c8a39c3
Plot <49232758+ldstr@users.noreply.github.com>
2024-12-05 11:58:09 +1000

    Create RemoveRequestBody.cs

---
+++ remov-backend/Remov.Backend/Models/RemoveRequestBody.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Remov.Backend.Models;
+
+[JsonSerializable(typeof(RemoveRequestBody))]
+internal partial class RemoveRequestBodyContext : JsonSerializerContext;
+
+public class RemoveRequestBody
+{
+    [JsonPropertyName("email")]
+    public string? Email { get; set; }
+
+    [JsonPropertyName("track_uri")]
+    public string? TrackUri { get; set; }
+
+    [JsonPropertyName("playlist_uri")]
+    public string? PlaylistUri { get; set; }
+
+    [JsonPropertyName("captcha_response")]
+    public string? CaptchaResponse { get; set; }
+}
