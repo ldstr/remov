76434ea3449130a7d17eeddde612805a6eb7ca50
Plot <49232758+ldstr@users.noreply.github.com>
2024-12-05 16:26:18 +1000

    Update src

--- remov-frontend/src/app/page.tsx
+++ remov-frontend/src/app/page.tsx
@@ -1,101 +1,163 @@
-import Image from "next/image";
+'use client';
+
+import Image from 'next/image';
+import Link from 'next/link';
+import { clsx } from 'clsx';
+import { FormEvent } from 'react';
+
+const Input = ({
+  id,
+  label,
+  placeholder
+}: {
+  id: string;
+  label: string;
+  placeholder: string;
+}) => (
+  <div>
+    <label className='block font-medium text-gray-100' htmlFor={id}>
+      {label}
+    </label>
+
+    <input
+      id={id}
+      type='text'
+      className={clsx(
+        'bg-[#121212] border border-gray-500',
+        'mt-1 block w-full p-3 rounded shadow-sm placeholder-gray-400 sm:text-sm',
+        'focus:outline-none focus:ring-gray-100 focus:border-gray-100 focus:ring-2',
+        'transition delay-100 duration-150 ease-in-out'
+      )}
+      placeholder={placeholder}
+    />
+  </div>
+);
+
+const NewTabLink = ({ text, href }: { text: string; href: string }) => (
+  <Link className='underline' href={href} target='_blank'>
+    {text}
+  </Link>
+);
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const removeTrack = async (e: FormEvent) => {
+    e.preventDefault();
+    alert('nigga bob!');
+  };
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+  return (
+    <div
+      className={clsx(
+        'bg-[#121212] sm:bg-gradient-to-b sm:from-[#2A2A2A] sm:to-black',
+        'flex items-center justify-center min-h-screen'
+      )}>
+      <form
+        className='bg-[#121212] py-8 px-12 sm:rounded-xl sm:shadow-md w-full max-w-xl'
+        onSubmit={removeTrack}>
+        <div className='sm:px-16 sm:mt-6'>
+          <div className='text-center space-y-2 mb-8'>
+            <h2 className='text-2xl font-bold text-white'>Remov.gg</h2>
+            <p className='text-gray-100'>Instant and Automated Track Removal</p>
+          </div>
+
+          <div className='space-y-4 mb-6'>
+            <Input id='name' label='Name' placeholder='Your full name' />
+
+            <div>
+              <Input id='email' label='Email' placeholder='Your email' />
+
+              <button
+                className='text-white underline text-sm mt-1'
+                type='button'
+                onClick={() =>
+                  alert(
+                    'We need your name and email to confirm the authenticity of the request ' +
+                      'and prevent misuse of the service. Your data will not be shared with anyone else.'
+                  )
+                }>
+                Why do we need this?
+              </button>
+            </div>
+
+            <Input
+              id='track-link'
+              label='Track Link'
+              placeholder='The link/URI of the track'
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+
+            <Input
+              id='playlist-link'
+              label='Playlist Link'
+              placeholder='The link/URI of the playlist'
+            />
+
+            <div className='flex items-center space-x-4'>
+              <div className='h-5 w-5'>
+                <input
+                  id='confirm-checkbox'
+                  type='checkbox'
+                  className={clsx(
+                    'appearance-none h-5 w-5 rounded-md',
+                    'border border-gray-500 bg-[#121212]',
+                    'checked:bg-primary checked:border-primary hover:border-primary',
+                    'transition delay-100 duration-150 ease-in-out'
+                  )}
+                />
+              </div>
+
+              <label
+                htmlFor='confirm-checkbox'
+                className='text-sm text-gray-200 select-none'>
+                I confirm that the track I want removed is mine or belongs to an
+                artist I manage or represent.
+              </label>
+            </div>
+          </div>
+
+          <button
+            type='submit'
+            className={clsx(
+              'bg-primary text-black font-medium',
+              'hover:bg-primary-hover hover:scale-105',
+              'w-full py-3 px-4 rounded-full',
+              'focus:outline-none focus:ring-1 focus:ring-gray-100 focus:ring-offset-2',
+              'transition duration-100 ease-in-out'
+            )}>
+            Remove Me!
+          </button>
+
+          <div className='px-5 py-4 mt-4 text-gray-300'>
+            <div className='flex justify-center items-center space-x-4'>
+              <Image
+                src='/recaptcha-icon.svg'
+                width={32}
+                height={32}
+                alt='ReCaptcha Logo'
+              />
+
+              <p className='text-xs'>
+                This site is protected by reCAPTCHA and the Google{' '}
+                <NewTabLink
+                  text='Privacy Policy'
+                  href='https://policies.google.com/privacy'
+                />{' '}
+                and{' '}
+                <NewTabLink
+                  text='Terms of Service'
+                  href='https://policies.google.com/terms'
+                />{' '}
+                apply.
+              </p>
+
+              <br />
+            </div>
+
+            <p className='text-xs mt-6 text-center'>
+              WE ARE NOT AFFILIATED WITH SPOTIFY IN ANY WAY.
+            </p>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </form>
     </div>
   );
 }
--- remov-frontend/tailwind.config.ts
+++ remov-frontend/tailwind.config.ts
@@ -1,18 +1,32 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+        primary: {
+          DEFAULT: '#1ED760',
+          hover: '#3AE376'
+        },
+        gray: {
+          50: '#F9F9F9', // Very light gray
+          100: '#F2F2F2', // Light gray
+          200: '#E5E5E5', // Lighter gray
+          300: '#CCCCCC', // Light-medium gray
+          400: '#A6A6A6', // Medium gray
+          500: '#7C7C7C', // Your custom gray
+          600: '#666666', // Dark-medium gray
+          700: '#4D4D4D', // Dark gray
+          800: '#333333', // Very dark gray
+          900: '#1A1A1A' // Almost black
+        }
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 } satisfies Config;
