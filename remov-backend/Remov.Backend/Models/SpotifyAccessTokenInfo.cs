ea34083ad413fd45d701f8ac9ee64bd8810f48af
Plot <49232758+ldstr@users.noreply.github.com>
2024-12-05 16:27:04 +1000

    Create SpotifyAccessTokenInfo.cs

---
+++ remov-backend/Remov.Backend/Models/SpotifyAccessTokenInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Remov.Backend.Models;
+
+[JsonSerializable(typeof(SpotifyAccessTokenInfo))]
+internal partial class SpotifyAccessTokenContext : JsonSerializerContext;
+
+public class SpotifyAccessTokenInfo
+{
+    [JsonPropertyName("accessToken")]
+    public string? AccessToken { get; set; }
+
+    [JsonPropertyName("accessTokenExpirationTimestampMs")]
+    public long ExpirationTimestampMs { get; set; }
+
+    [JsonPropertyName("isAnonymous")]
+    public bool IsAnonymous { get; set; }
+
+    [JsonPropertyName("clientId")]
+    public string? ClientId { get; set; }
+}
